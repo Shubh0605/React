15a4845f11f26e659fae862dab45e529b28c0c7a
shubh012 <shubprajapati012@gmail.com>
2024-12-19 16:35:33 +0530

    commit Episode6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,26 @@
 import Restaurantcard from "./Restaurantcard";
 
 import resList from "../utils/mockdata";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const Body = () => {
+  const [ListOfrestaurants, setListOfrestaurants] = useState(resList);
 
-  const [ListOfrestaurants,setListOfrestaurants] = useState(resList);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    console.log("13 line Called");
+    const data = await fetch(
+    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.9615398&lng=79.2961468&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    console.log();
+    const json = await data.json();
+    setListOfrestaurants(
+    json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    );
+    console.log(ListOfrestaurants);
+   };
 
   // var ListOfrestaurants = [
   //   {
@@ -253,27 +269,28 @@ const Body = () => {
   //     subtype: "basic",
   //   },
   // ];
-
-    return (
-      <div className="body">
-        <div className="filter">
-          <button className="filter-btn" onClick={() => {
-
-           filterlist = ListOfrestaurants.filter((resList) => resList.data.avgRating > 4 );
-           setListOfrestaurants(filterlist);
-          }}>
-            Top Rated Restaurants</button>
-        </div>
-        <div className="res-container">
-          {ListOfrestaurants.map((restaurant) => (
-            <Restaurantcard key={restaurant.data.id} resData={restaurant} />
-            
-          )
-          
-
-          )}
-        </div>
+  if(!ListOfrestaurants) return
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filterlist = ListOfrestaurants.filter(
+              (resList) => resList.data.avgRating > 4
+            );
+            setListOfrestaurants(filterlist);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
       </div>
-    )
-  }
-  export default Body;
+      <div className="res-container">
+        {ListOfrestaurants?.map((restaurant) => (
+          <Restaurantcard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Body;
