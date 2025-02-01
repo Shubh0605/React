8fe066cdf295157ae54751e4e2eff540fc040834
shubh012 <shubprajapati012@gmail.com>
2025-02-02 00:59:16 +0530

    Fetch and display user information from GitHub API in UserClass component

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,29 +2,62 @@ import React from 'react';
 class UserClass extends React.Component{
   constructor(props){
     super(props);
+    // this.state = {
+    //     count: 0,
+    //     count2: 2
+    // }
+
     this.state = {
-        count: 0,
-        count2: 2
+       userInfo: {
+        name: "Dummy",
+        location: "Defaut",
+       },
     };
- }
+
+   
+ 
+}
+async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/shubh012");
+    const json = await data.json();
+
+    this.setState({
+        userInfo: json,
+
+
+    });
+    console.log(json);
+    
+
+
+}
     render(){
-        const {name, location} = this.props;
-        const {count,count2} = this.state;
+        const {name, location, avatar_url} = this.state.userInfo;
+        // const {name, location} = this.props;
+        // const {count,count2} = this.state;
         return(
-            <div className="user-card">
-                <h1>Count: {count}</h1>
-                <h1>Count 2: {count2}</h1>
-                <button onClick={() => {
-                    //never update state variables directly
-                    this.setState({count: count + 1, count2: count2 + 1});
+        //     <div className="user-card">
+        //      {/* <h1>Count: {count}</h1>
+        //         <h1>Count 2: {count2}</h1>
+        //         <button onClick={() => {
+        //             //never update state variables directly
+        //             this.setState({count: count + 1, count2: count2 + 1});
                     
 
-                }}>Count Inc</button>
+        //         }}>Count Inc</button> */}
             
+        //     <h2>Name: {name}</h2>
+        //     <h3>Location: {location}</h3>
+        //     <h4>Contact: 7303411297</h4>
+        // </div>
+    
+        <div className='user-card'>
+            <img src={avatar_url}/>
             <h2>Name: {name}</h2>
             <h3>Location: {location}</h3>
             <h4>Contact: 7303411297</h4>
         </div>
+        
 
         )
     }
